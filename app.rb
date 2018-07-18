25dd3a75911fda8c800dbf13875069ce388e4f58
Layth <laythq@gmail.com>
2018-07-18 10:25:14 +0100

    Authentication Route

--- app.rb
+++ app.rb
@@ -3,9 +3,31 @@ require './lib/user'
 require './lib/listing'
 
 class MakersBNB < Sinatra::Base
+  set :method_override, true
+  enable :sessions
 
   get '/' do
-    'Hello world!'
+    # Sign-in page
+    erb :index
+  end
+
+  get '/:id/listings' do
+    erb :listings
+  end
+
+  post '/:id/session' do
+    # Authenticate User, redirect to listings...
+    user = User.authenticate(params[:username, params[:password]])
+
+    if user
+      # If authentication successful, assign sesssion 'id' to this user's id.
+      session[:id] = user.id
+      redirect '/:id/listings'
+    else
+      # Otherwise, return to sign in page
+      redirect '/'
+
+    redirect '/:id/listings'
   end
 
   run! if app_file == $0
