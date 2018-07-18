cc1319542b743628c2d086f8953018d7a6830bc8
Layth <laythq@gmail.com>
2018-07-18 14:19:03 +0100

    basic listings page

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,16 @@
-feature 'Test' do
+feature 'Homepage' do
   scenario 'Test infrastructure works' do
     visit ('/')
-    expect(page).to have_content 'Hello world'
+    expect(page).to have_content 'Welcome to MakersBnB'
+  end
+end
+
+feature 'Sign In' do
+  scenario 'User signs in' do
+    visit('/')
+    fill_in('username', :with => 'Daniel')
+    fill_in('password', :with => 'pa$$word1')
+    click_button('Submit')
+    expect(page).to have_content 'Welcome, Daniel'
   end
 end
