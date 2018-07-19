56931a8a3f41a6de76fd7a333477dadfcbcedc38
Layth <laythq@gmail.com>
2018-07-19 15:55:27 +0100

    Updated Spec

--- spec/features/approveRequestSpec.rb
+++ spec/features/approveRequestSpec.rb
@@ -15,4 +15,21 @@ feature 'Approve Request' do
     click_button('Back to My Listings')
     expect(page).to have_content '(approved)'
   end
+
+  scenario 'User rejects a request' do
+    visit('/')
+    fill_in('username', :with => 'Daniel')
+    fill_in('password', :with => 'pa$$word1')
+    click_button('Submit')
+    click_button('Request', :match => :first)
+    click_button('Back to Listings')
+    click_button('Sign Out')
+    fill_in('username', :with => 'Layth')
+    fill_in('password', :with => 'pa$$w0rd2')
+    click_button('Submit')
+    click_button('My Listings')
+    click_button('Approve')
+    click_button('Back to My Listings')
+    expect(page).to have_content '(rejected)'
+  end
 end
